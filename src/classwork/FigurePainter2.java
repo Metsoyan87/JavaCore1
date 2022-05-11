d3401ce878eff25e4c17425de0f8a66694119bb1
Metsoyan87 <bunit96@mail.ru>
2022-05-11 10:09:53 +0400

    First commit

--- src/classwork/FigurePainter2.java
+++ src/classwork/FigurePainter2.java
@@ -20,5 +20,15 @@ public class FigurePainter2 {
             }
             System.out.println();
         }
+        System.out.println();
+        for (int i = 5; i > 0; i--) {
+            for (int j = 5; j > i; j--) {
+                System.out.print("  ");
+            }
+            for (int j = 0; j < i; j++) {
+                System.out.print(" *");
+            }
+            System.out.println();
+        }
     }
 }
