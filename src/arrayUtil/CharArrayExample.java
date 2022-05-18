ebe26397cc9a3dd3111abb4dcb64aa4fdc8c0477
Mnatsakan <105308500+Metsoyan87@users.noreply.github.com>
2022-05-18 22:53:04 +0400

    Update CharArrayExample.java

--- src/arrayUtil/CharArrayExample.java
+++ src/arrayUtil/CharArrayExample.java
@@ -12,13 +12,17 @@ public class CharArrayExample {
             }
         }
         System.out.println("c-փոփոխականից կա " + n + " հատ ");
+
         //2.պետք է գրել կոդ, որը կտպի մեջտեղի 2 սինվոլները։
+
         char[] chars2 = {'j', 'a', 'v', 'a', 'l', 'o', 'v', 'e' };
         int x = (chars2.length-1) / 2;
         System.out.print(chars2[x] );
         System.out.print(chars2[x + 1]);
         System.out.println();
+
         //3.պետք է տպենք true եթե մեր մասիվը վերջանում է ly-ով, եթե ոչ՝ false
+
         char[] chars3 = {'j', 'a', 'v', 'a', 'l', 'o', 'l', 'y' };
         int i = chars3.length - 1;
         boolean b = false;
@@ -26,12 +30,14 @@ public class CharArrayExample {
             b = true;
         }
         System.out.println(b);
+
         //4.Տպում ենք true եթե տեքստը պարունակում է bob բառը, բայց o-ի տեղը կարող է լինել ցանկացած սինվոլ։
+
         char[] bobArray = {'b', 'a', 'b', 'o', 'l', 'a' };
         boolean isBob = false;
-        char bob = 'b';
+        char b1 = 'b';
         for (int i1 = 0; i1 < bobArray.length; i++) {
-            if (i1 + 2 < bobArray.length && bobArray[i1] == bob && bobArray[i1 + 2] == bob) {
+            if (i1 + 2 < bobArray.length && bobArray[i1] == b1 && bobArray[i1 + 2] == b1) {
                 isBob = true;
                 break;
             }
@@ -39,6 +45,7 @@ public class CharArrayExample {
         System.out.println(isBob);
 
         //5.պետք է տպել մասիվի այն սինվոլները որոնք պռաբել չեն.
+
         char[] text = {' ', ' ', 'b', 'a', 'r', 'e', 'v', ' ', ' ' };
         for (int j = 0; j < text.length; j++) {
             if (text[j] != ' ') {
@@ -47,3 +54,6 @@ public class CharArrayExample {
         }
     }
 }
+
+
+
