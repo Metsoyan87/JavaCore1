17d65a56e33a511af0b1552557afa2b28d3285d1
Mnatsakan <105308500+Metsoyan87@users.noreply.github.com>
2022-05-18 22:33:47 +0400

    Update CharArrayExample.java

--- src/arrayUtil/CharArrayExample.java
+++ src/arrayUtil/CharArrayExample.java
@@ -9,47 +9,41 @@ public class CharArrayExample {
         for (int i = 0; i < chars.length; i++) {
             if (chars[i] == c) {
                 n++;
-                System.out.println("c-փոփոխականից կա " + n + " հատ ");
             }
         }
-
+        System.out.println("c-փոփոխականից կա " + n + " հատ ");
         //2.պետք է գրել կոդ, որը կտպի մեջտեղի 2 սինվոլները։
         char[] chars2 = {'j', 'a', 'v', 'a', 'l', 'o', 'v', 'e' };
-        for (int i = 3; i < chars2.length - 3; i++) {
-            System.out.print(chars2[i] + " ");
-        }
-
-
+        int x = (chars2.length-1) / 2;
+        System.out.print(chars2[x] );
+        System.out.print(chars2[x + 1]);
+        System.out.println();
         //3.պետք է տպենք true եթե մեր մասիվը վերջանում է ly-ով, եթե ոչ՝ false
         char[] chars3 = {'j', 'a', 'v', 'a', 'l', 'o', 'l', 'y' };
-        if (chars3[6] == 'l' && chars3[7] == 'y') {
-            System.out.println(true);
-
-        } else System.out.println(false);
-
-
+        int i = chars3.length - 1;
+        boolean b = false;
+        if (chars3[i] == 'y' && chars3[i - 1] == 'l') {
+            b = true;
+        }
+        System.out.println(b);
         //4.Տպում ենք true եթե տեքստը պարունակում է bob բառը, բայց o-ի տեղը կարող է լինել ցանկացած սինվոլ։
         char[] bobArray = {'b', 'a', 'b', 'o', 'l', 'a' };
-        for (int i = 0; i < bobArray.length; i++) {
-            char a = 'b';
-            char s = 'o';
-            char d = 'b';
-            if (bobArray[i] == a || bobArray[i] == s || bobArray[i] == d) {
-                System.out.println(true);
+        boolean isBob = false;
+        char bob = 'b';
+        for (int i1 = 0; i1 < bobArray.length; i++) {
+            if (i1 + 2 < bobArray.length && bobArray[i1] == bob && bobArray[i1 + 2] == bob) {
+                isBob = !isBob;
                 break;
             }
-            System.out.println(false);
         }
-
+        System.out.println(isBob);
 
         //5.պետք է տպել մասիվի այն սինվոլները որոնք պռաբել չեն.
         char[] text = {' ', ' ', 'b', 'a', 'r', 'e', 'v', ' ', ' ' };
-        for (int i = 0; i < text.length; i++) {
-            if (text[i] != ' ') {
-                System.out.print(text[i]);
+        for (int j = 0; j < text.length; j++) {
+            if (text[j] != ' ') {
+                System.out.print(text[j]);
             }
         }
     }
 }
-
-
