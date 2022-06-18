6c7e1d91f119b8953228b300fb23609415cc606c
Mnatsakan <105308500+Metsoyan87@users.noreply.github.com>
2022-06-18 18:55:54 +0400

    Update PersonTest.java

--- src/Tests/PersonTest.java
+++ src/Tests/PersonTest.java
@@ -3,13 +3,6 @@ package Tests;
 public class PersonTest {
     public static void main(String[] args) {
 
-        Person person = new Person(176.3, "Gago", "Sargsyan");
-        person.show(18);
-
-        Person person1 = new Person(169.2,"Vlad", "Ginosyan");
-        person1.show(29);
-
-        Person person2 = new Person(160.8,"Saten", "popozyan");
-        person2.show(45);
+       
     }
 }
