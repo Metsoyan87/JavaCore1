270064dc0f0f8e4d4b51bd3d3cfc9a478a20e8d2
Mnatsakan <105308500+Metsoyan87@users.noreply.github.com>
2022-06-18 18:55:06 +0400

    Update Person.java

--- src/Tests/Person.java
+++ src/Tests/Person.java
@@ -1,21 +1,58 @@
 package Tests;
 
 public class Person {
-
-    double height;
     String name;
     String surname;
+    double age;
+    double growth;
+
+    Person(Person ob) {
+        this.name = ob.name;
+        this.surname = ob.surname;
+        this.age = ob.age;
+        this.growth = ob.growth;
+    }
 
     Person() {
     }
+    public Person(String name, String surname, double age, double growth) {
+        this.name = name;
+        this.surname = surname;
+        this.age = age;
+        this.growth = growth;
+    }
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
 
-    public Person(double h, String n, String s) {
-        this.height = h;
-        this.name = n;
-        this.surname = s;
+        Person person = (Person) o;
+
+        if (Double.compare(person.age, age) != 0) return false;
+        if (Double.compare(person.growth, growth) != 0) return false;
+        if (name != null ? !name.equals(person.name) : person.name != null) return false;
+        return surname != null ? surname.equals(person.surname) : person.surname == null;
+    }
+    @Override
+    public int hashCode() {
+        int result;
+        long temp;
+        result = name != null ? name.hashCode() : 0;
+        result = 31 * result + (surname != null ? surname.hashCode() : 0);
+        temp = Double.doubleToLongBits(age);
+        result = 31 * result + (int) (temp ^ (temp >>> 32));
+        temp = Double.doubleToLongBits(growth);
+        result = 31 * result + (int) (temp ^ (temp >>> 32));
+        return result;
+    }
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                ", age=" + age +
+                ", growth=" + growth +
+                '}';
     }
 
-    void show(int age) {
-        System.out.println("height: " + height + " " + "name: " + name + " " + "surname: " + surname + "age: = " + age);
-    }
 }
