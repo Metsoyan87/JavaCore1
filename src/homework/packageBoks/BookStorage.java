46c3387e1bf806c52f02c0c19a5377f4c19e96d1
Mnatsakan <105308500+Metsoyan87@users.noreply.github.com>
2022-06-26 21:30:21 +0400

    Add files via upload

---
+++ src/homework/packageBoks/BookStorage.java
@@ -0,0 +1,60 @@
+package homework.packageBoks;
+
+import homework.students.Student;
+
+public class BookStorage {
+    Book[] array = new Book[10];
+    int size = 0;
+
+    void add(Book book) {
+        if (size == array.length) {
+            extend();
+        }
+        array[size++] = book;
+    }
+
+    private void extend() {
+        Book[] temp = new Book[array.length + 10];
+        for (int i = 0; i < size; i++) {
+            temp[i] = array[i];
+        }
+        array = temp;
+    }
+
+    public void print() {
+        for (int i = 0; i < size; i++) {
+            System.out.println(i + ". " + array[i] + " ");
+        }
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void printBooksByAuthorName(String authorName) {
+
+        for (int i = 0; i < size; i++) {
+            if (array[i].getAuthorName().equalsIgnoreCase(authorName)) {
+                System.out.println(array[i]);
+            }
+        }
+    }
+
+    public void printBooksByGenre(String genre) {
+        for (int i = 0; i < size; i++) {
+            if (array[i].getGenre().equalsIgnoreCase(genre)) {
+                System.out.println(array[i]);
+            }
+        }
+    }
+
+    public void printBooksByPriceRange(double price, double range) {
+
+        for (int i = 0; i < size; i++) {
+            if (array[i].getPrice() > price && array[i].getPrice() > range) {
+                System.out.println(array[i]);
+            }
+        }
+    }
+}
+
---
+++ src/homework/packageBoks/Commands.java
@@ -0,0 +1,12 @@
+package homework.packageBoks;
+
+public interface Commands {
+
+    int EXIT = 0;
+    int ADD_BOOK = 1;
+    int PRINT_ALL_Books = 2;
+    int PRINT_BOOKS_BY_AUTHOR_NAME = 3;
+    int PRINT_BOOKS_BY_GENRE = 4;
+    int PRINT_BOOKS_BY_PRICE_RANGE = 5;
+
+}
