e60a2d3e133b87dce3c3e17f30538f93e3040737
Mnatsakan <105308500+Metsoyan87@users.noreply.github.com>
2022-07-04 22:56:45 +0400

    Add files via upload

---
+++ src/homework/packageBook/commands/Commands.java
@@ -0,0 +1,27 @@
+package homework.packageBook.commands;
+
+public interface Commands {
+
+    int EXIT = 0;
+    int ADD_BOOK = 1;
+    int PRINT_ALL_Books = 2;
+    int PRINT_BOOKS_BY_AUTHOR_NAME = 3;
+    int PRINT_BOOKS_BY_GENRE = 4;
+    int PRINT_BOOKS_BY_PRICE_RANGE = 5;
+    int ADD_AUTHOR = 6;
+    int PRINT_ALL_AUTHORS = 7;
+    int SEARCH_AUTHOR_BY_INDEX = 8;
+
+    static void printCommand(){
+        System.out.println("please input " + EXIT + "  for exit");
+        System.out.println("please input " + ADD_BOOK + " for add book");
+        System.out.println("please input " + PRINT_ALL_Books + " for print all books");
+        System.out.println("please input " + PRINT_BOOKS_BY_AUTHOR_NAME + " for print books by author name");
+        System.out.println("please input " + PRINT_BOOKS_BY_GENRE + " for print by genre");
+        System.out.println("please input " + PRINT_BOOKS_BY_PRICE_RANGE + " for print by price range");
+        System.out.println("please input " + ADD_AUTHOR + " for add author");
+        System.out.println("please input " + PRINT_ALL_AUTHORS + " for print all authors");
+        System.out.println("please input " + SEARCH_AUTHOR_BY_INDEX + " for search by author");
+
+    }
+}
---
+++ src/homework/packageBook/model/Book.java
@@ -0,0 +1,73 @@
+package homework.packageBook.model;
+
+public class Book {
+
+    private String title;
+    private Author author;
+    private double price;
+    private int count;
+    private String genre;
+
+
+    public Book() {
+    }
+
+    public Book(String title, double price, int count, String genre, Author author) {
+        this.title = title;
+        this.price = price;
+        this.count = count;
+        this.genre = genre;
+        this.author = author;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public Author getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(Author author) {
+        this.author = author;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "title='" + title + '\'' +
+                ", author=" + author.getName() +
+                ", price=" + price +
+                ", count=" + count +
+                ", genre='" + genre + '\'' +
+                '}';
+    }
+}
