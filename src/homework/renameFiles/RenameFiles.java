e88a088e58d40a50ca44a9ddbdf80865440929f6
Mnatsakan <105308500+Metsoyan87@users.noreply.github.com>
2022-07-18 20:52:22 +0400

    Update RenameFiles.java

--- src/homework/renameFiles/RenameFiles.java
+++ src/homework/renameFiles/RenameFiles.java
@@ -4,7 +4,7 @@ import java.io.File;
 
 public class RenameFiles {
 
-    public static final String PATH = "E:\\Heraxos Vika\\Nkar";
+    public static final String PATH = "E:\\";  //E:\\Պապկի անունը
 
     public static void main(String[] args) {
         
@@ -14,8 +14,10 @@ public class RenameFiles {
         for (int i = 0; i < files.length; i++) {
 
             if (files[i].isFile()) {
-                File f = new File("E:\\Heraxos Vika\\Nkar\\" + files[i].getName());
-                f.renameTo(new File("E:\\Heraxos Vika\\Nkar\\" + "IMG_" + i + ".jpg"));
+                File f = new File("E:\\" + files[i].getName());              //E:\\Պապկի անունը
+
+                f.renameTo(new File("E:\\" + "IMG_" + i + ".jpg"));         //E:\\Պապկի անունը
+
             }
         }
     }
